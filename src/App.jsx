0887c88b1b17002b4a1cdfdfb62b291296c96d99
Henrique Schroeder <henriquevschdev@gmail.com>
2022-08-15 21:13:07 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Calculator from './components/main/Calculator.jsx'
+import Footer from './components/Footer.jsx'
+
+function App() {
+  return (
+    <>
+      <h1>Calculator.js</h1>
+      <h3>The classic one.</h3>
+      <Calculator />
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Footer.css'
+
+export default () => {
+  return (
+    <footer>
+      <div>
+        <h3>
+          Developed with <span>☕</span> by
+          <a href="https://github.com/henriquevschroeder" target="_blank">
+            @henriquevschroeder
+          </a>
+        </h3>
+      </div>
+    </footer>
+  )
+}
